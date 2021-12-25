ef492b5b0201f0be5f266c3142d4137c1109585a
Ninowicz <62292303+Ninowicz@users.noreply.github.com>
2021-12-25 19:32:36 +0100

    ArmaTRon_Key

--- ArmageddonTronTest/ArmageddonTron/Script/Build/Script.js
+++ ArmageddonTronTest/ArmageddonTron/Script/Build/Script.js
@@ -42,12 +42,32 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
+    let fps = 60;
+    let graph;
+    let agent;
+    let ctrForward = new ƒ.Control("Forward", 10, 0 /* PROPORTIONAL */);
+    ctrForward.setDelay(200);
     function start(_event) {
         viewport = _event.detail;
+        viewport.camera.mtxPivot.translateY(200);
+        viewport.camera.mtxPivot.rotateX(90);
+        graph = viewport.getBranch();
+        agent = graph.getChildrenByName("Bike")[0];
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps);
     }
     function update(_event) {
+        let deltaTime = ƒ.Loop.timeFrameReal / 1000;
+        //let agentRotationSpeed: number = 250;
+        let value = (ƒ.Keyboard.mapToValue(-1, 0, [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])
+            + ƒ.Keyboard.mapToValue(1, 0, [ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]));
+        ctrForward.setInput(value * deltaTime);
+        agent.mtxLocal.translateZ(ctrForward.getOutput());
+        // agent.mtxLocal.translateY(-speedAgentTranslation * deltaTime);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]))
+            agent.mtxLocal.rotateY(-90);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
+            agent.mtxLocal.rotateY(90);
         // ƒ.Physics.world.simulate();  // if physics is included and used
         viewport.draw();
         ƒ.AudioManager.default.update();
--- ArmageddonTronTest/ArmageddonTron/Script/Source/Main.ts
+++ ArmageddonTronTest/ArmageddonTron/Script/Source/Main.ts
@@ -5,20 +5,40 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
   let fps: number = 60;
-  let root: ƒ.Node;
+  let graph: ƒ.Node;
+  let agent: ƒ.Node;
+  let ctrForward: ƒ.Control = new ƒ.Control("Forward", 10, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  ctrForward.setDelay(200);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
-    root = viewport.getBranch();
+    viewport.camera.mtxPivot.translateY(200);
+    viewport.camera.mtxPivot.rotateX(90);
+    graph = viewport.getBranch();
     
-    viewport.camera.mtxPivot.translateZ(-60);
+    agent = graph.getChildrenByName("Bike")[0];
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps); 
   }
 
   function update(_event: Event): void {
+
+    let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;
+    //let agentRotationSpeed: number = 250;
+
+    let value: number = (
+      ƒ.Keyboard.mapToValue(-1, 0, [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])
+      + ƒ.Keyboard.mapToValue(1, 0, [ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP])
+    );
+    ctrForward.setInput(value * deltaTime);
+    agent.mtxLocal.translateZ(ctrForward.getOutput());
+
+    // agent.mtxLocal.translateY(-speedAgentTranslation * deltaTime);
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]))
+      agent.mtxLocal.rotateY(-90);
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
+      agent.mtxLocal.rotateY(90);
     // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
