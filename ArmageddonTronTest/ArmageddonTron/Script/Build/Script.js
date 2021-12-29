f23c953f878239c34b507280f676a51c1a5132a8
Ninowicz <62292303+Ninowicz@users.noreply.github.com>
2021-12-30 00:45:01 +0100

    ArmaTron_CameraAlpha

--- ArmageddonTronTest/ArmageddonTron/Script/Build/Script.js
+++ ArmageddonTronTest/ArmageddonTron/Script/Build/Script.js
@@ -42,11 +42,22 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
+    //test for camera 
+    let camera = new ƒ.Node("cameraNode");
+    let cmpCamera = new ƒ.ComponentCamera;
     let fps = 60;
     let graph;
     let agent;
     let ctrForward = new ƒ.Control("Forward", 10, 0 /* PROPORTIONAL */);
     ctrForward.setDelay(200);
+    let CameraControl = new ƒ.Control("Forward", 10, 0 /* PROPORTIONAL */);
+    //CameraControl.setDelay(500);
+    let RotationCameraTest_Left = false;
+    let TheChosenOne_Left = 1;
+    let RotationCameraTest_Right = false;
+    let TheChosenOne_Right = -1;
+    let LongRun = false;
+    let MemorieNumber = 0;
     let Referee_Left;
     let Referee2_Left;
     let Referee_Right;
@@ -63,15 +74,66 @@ var Script;
     StartKey = false;
     function start(_event) {
         viewport = _event.detail;
-        viewport.camera.mtxPivot.translateY(150);
-        viewport.camera.mtxPivot.rotateX(90);
+        viewport.calculateTransforms();
+        //viewport.camera.mtxPivot.translateY(150);
+        //viewport.camera.mtxPivot.rotateX(90);
         graph = viewport.getBranch();
         agent = graph.getChildrenByName("Bike")[0];
+        cmpCamera.mtxPivot.translation = new ƒ.Vector3(0, 6, -16); // 0 8 -12
+        cmpCamera.mtxPivot.rotation = new ƒ.Vector3(18, 0, 0);
+        camera.addComponent(cmpCamera);
+        camera.addComponent(new ƒ.ComponentTransform());
+        graph.addChild(camera);
+        let canvas = document.querySelector("canvas");
+        viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", graph, cmpCamera, canvas);
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps);
     }
     function update(_event) {
+        // Camera left turn
+        if (RotationCameraTest_Left == true) {
+            TheChosenOne_Left = 1;
+            CameraControl.setInput(0.5 * TheChosenOne_Left);
+            camera.mtxLocal.rotateY(CameraControl.getOutput());
+            if (Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) < 1 || Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) > 180) {
+                RotationCameraTest_Left = false;
+                TheChosenOne_Left = 0;
+                camera.mtxLocal.rotation.y = agent.mtxLocal.rotation.y;
+                LongRun = false;
+            }
+        }
+        // Camera right turn
+        if (RotationCameraTest_Right == true) {
+            TheChosenOne_Right = -1;
+            CameraControl.setInput(0.5 * TheChosenOne_Right);
+            camera.mtxLocal.rotateY(CameraControl.getOutput());
+            if (Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) < 1 || Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) > 180) {
+                RotationCameraTest_Right = false;
+                TheChosenOne_Right = 0;
+                camera.mtxLocal.rotation.y = agent.mtxLocal.rotation.y;
+                LongRun = false;
+            }
+        }
+        // Camera goes back to normal
+        if (KeyStatus_Left == true && KeyStatus_Right == true) {
+            MemorieNumber = MemorieNumber + 1;
+            if (MemorieNumber > 100) {
+                camera.mtxLocal.rotation.y = agent.mtxLocal.rotation.y;
+                MemorieNumber = 0;
+            }
+        }
+        else {
+            MemorieNumber = 0;
+        }
+        console.log(MemorieNumber);
+        camera.mtxLocal.translation = agent.mtxWorld.translation;
+        //if (camera.mtxLocal.translation != agent.mtxWorld.translation){
+        //  camera.mtxLocal.translation = new ƒ.Vector3(0,agent.mtxWorld.translation.y,-12);
+        //}
+        //camera.mtxLocal.rotation = new ƒ.Vector3(0, agent.mtxWorld.rotation.y, 0);
         let deltaTime = ƒ.Loop.timeFrameReal / 1000;
+        //---------- Movement Managment ----------
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP])) {
             StartKey = true;
         }
@@ -81,14 +143,18 @@ var Script;
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]) && KeyStatus_Left == true) {
             agent.mtxLocal.rotateY(90);
+            //TheChosenOne_Left = -1;
             KeyStatus_Left = false;
+            RotationCameraTest_Left = true;
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]) && KeyStatus_Right == true) {
             agent.mtxLocal.rotateY(-90);
+            //TheChosenOne_Left = 1;
             KeyStatus_Right = false;
+            RotationCameraTest_Right = true;
         }
         Referee2_Left = Referee_Left;
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) { // || ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])){
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
             Referee_Left = Referee_Left + 1;
         }
         if (Referee2_Left == Referee_Left) {
@@ -109,7 +175,7 @@ var Script;
             Referee_Right = 0;
             Referee2_Right = -1;
         }
-        //console.log(Referee_Left);
+        //---------- End of Movement Managment
         // ƒ.Physics.world.simulate();  // if physics is included and used
         viewport.draw();
         ƒ.AudioManager.default.update();
--- ArmageddonTronTest/ArmageddonTron/Script/Source/Main.ts
+++ ArmageddonTronTest/ArmageddonTron/Script/Source/Main.ts
@@ -4,11 +4,26 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+
+  //test for camera 
+  let camera: ƒ.Node = new ƒ.Node("cameraNode");
+  let cmpCamera = new ƒ.ComponentCamera;
+
   let fps: number = 60;
   let graph: ƒ.Node;
   let agent: ƒ.Node;
   let ctrForward: ƒ.Control = new ƒ.Control("Forward", 10, ƒ.CONTROL_TYPE.PROPORTIONAL);
   ctrForward.setDelay(200);
+
+  let CameraControl =new ƒ.Control("Forward", 10, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  //CameraControl.setDelay(500);
+
+  let RotationCameraTest_Left : boolean = false;
+  let TheChosenOne_Left : number = 1;
+  let RotationCameraTest_Right : boolean = false;
+  let TheChosenOne_Right : number = -1;
+  let LongRun :boolean = false;
+  let MemorieNumber : number = 0;
 
   let Referee_Left:number;
   let Referee2_Left:number;
@@ -31,11 +46,23 @@ namespace Script {
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-    viewport.camera.mtxPivot.translateY(150);
-    viewport.camera.mtxPivot.rotateX(90);
+    viewport.calculateTransforms();
+    //viewport.camera.mtxPivot.translateY(150);
+    //viewport.camera.mtxPivot.rotateX(90);
     graph = viewport.getBranch();
     
     agent = graph.getChildrenByName("Bike")[0];
+
+    cmpCamera.mtxPivot.translation = new ƒ.Vector3(0,6,-16); // 0 8 -12
+    cmpCamera.mtxPivot.rotation = new ƒ.Vector3(18,0,0);
+    
+    camera.addComponent(cmpCamera);
+    camera.addComponent(new ƒ.ComponentTransform());
+    graph.addChild(camera);
+
+    let canvas: HTMLCanvasElement = document.querySelector("canvas");
+    viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", graph, cmpCamera, canvas);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps); 
@@ -43,7 +70,57 @@ namespace Script {
 
 
   function update(_event: Event): void {
-  
+    // Camera left turn
+    if(RotationCameraTest_Left == true ){
+      TheChosenOne_Left = 1;
+      CameraControl.setInput(0.5 * TheChosenOne_Left);
+      camera.mtxLocal.rotateY(CameraControl.getOutput());
+
+
+      if(Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) < 1 || Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) > 180){
+        
+        RotationCameraTest_Left = false;
+        TheChosenOne_Left = 0;
+        camera.mtxLocal.rotation.y = agent.mtxLocal.rotation.y;
+        LongRun = false;
+      }
+    }
+    // Camera right turn
+    if(RotationCameraTest_Right == true){
+      TheChosenOne_Right = -1;
+      CameraControl.setInput(0.5 * TheChosenOne_Right);
+      camera.mtxLocal.rotateY(CameraControl.getOutput());
+
+
+      if(Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) < 1 || Math.abs(camera.mtxLocal.rotation.y - agent.mtxLocal.rotation.y) > 180){
+        
+        RotationCameraTest_Right = false;
+        TheChosenOne_Right = 0;
+        camera.mtxLocal.rotation.y = agent.mtxLocal.rotation.y;
+        LongRun = false;
+      }
+    }
+
+    // Camera goes back to normal
+    if (KeyStatus_Left == true && KeyStatus_Right == true){
+      MemorieNumber = MemorieNumber + 1;
+      if(MemorieNumber > 100){
+        camera.mtxLocal.rotation.y = agent.mtxLocal.rotation.y;
+        MemorieNumber = 0;
+      }
+    }
+    else{
+      MemorieNumber = 0;
+    }
+    
+    console.log(MemorieNumber);
+    camera.mtxLocal.translation = agent.mtxWorld.translation;
+    //if (camera.mtxLocal.translation != agent.mtxWorld.translation){
+    //  camera.mtxLocal.translation = new ƒ.Vector3(0,agent.mtxWorld.translation.y,-12);
+    //}
+    
+    //camera.mtxLocal.rotation = new ƒ.Vector3(0, agent.mtxWorld.rotation.y, 0);
+   
     let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;
     
     
@@ -58,13 +135,18 @@ namespace Script {
     }  
       
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]) && KeyStatus_Left == true ){
-      agent.mtxLocal.rotateY(90);   
+      agent.mtxLocal.rotateY(90); 
+      //TheChosenOne_Left = -1;
       KeyStatus_Left = false;
+      RotationCameraTest_Left = true;
     }
         
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]) && KeyStatus_Right == true){
       agent.mtxLocal.rotateY(-90);
+      //TheChosenOne_Left = 1;
       KeyStatus_Right = false;
+      RotationCameraTest_Right = true;
+      
     }
     
     Referee2_Left = Referee_Left;
@@ -94,7 +176,7 @@ namespace Script {
     //---------- End of Movement Managment
 
 
-    
+
     // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
